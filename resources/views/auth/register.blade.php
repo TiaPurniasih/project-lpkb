d5a422d29df3e643974a7dca9457986fd7dfa820
giyazhaycal <46078343+giyazhaycal@users.noreply.github.com>
2025-12-03 22:39:48 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,6 @@
                                         </label>
                                         <input type="text" id="name" name="name" placeholder="info@gmail.com"
                                             class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
-                                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
                                     </div>
                                     <div>
                                         <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
@@ -34,7 +33,6 @@
                                         </label>
                                         <input type="email" id="email" name="email" placeholder="info@gmail.com"
                                             class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
-                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                         
                                     </div>
                                     <div>
@@ -63,7 +61,6 @@
                                                 </svg>
                                             </span>
                                         </div>
-                                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
                                     </div>
                                     <div>
                                         <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
@@ -91,7 +88,6 @@
                                                 </svg>
                                             </span>
                                         </div>
-                                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                                     </div>
                                   
                                     <div>
